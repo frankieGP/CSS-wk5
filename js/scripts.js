ab6cafe454e2c6fd871fa895f41dc070c6fb0e4f
Frankie Gavin <frankanthonygavin@gmail.com>
2016-12-21 16:38:55 -0800

    add addClass in JS to target rotating classes and ids

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
 $( document ).ready(function() {
-  $(".osc1").animate({left: '650px'});
 
+  $('[class*="osc1"]').on('click',function() {
+
+  $(this).addClass("osc1tail");
+
+  });
   $("#analog-bass").click(function() {
-
 
 
 
     var $this = $(this),
         audio = $this.children('audio')[0];
         audio.play();
+
 
 
   });
