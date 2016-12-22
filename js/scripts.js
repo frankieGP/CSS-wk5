c473b2d124115f77fc1b7cef0e5d1660dea5916a
Frankie Gavin <frankanthonygavin@gmail.com>
2016-12-21 23:41:11 -0800

    add sliders to animate on click for both sounds. the sliders will adjust automatically as new sound is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,55 @@ $("#analog-bass").on('click', function() {
       audio.play();
 
 
+      $( ".osc1-tail" ).addClass( "osc1-bass", 1000 );
+      $( ".osc2-tail" ).addClass( "osc2-bass", 1000 );
+      $( ".freq-tail" ).addClass( "freq-bass", 1000 );
+      $( ".res-tail" ).addClass( "res-bass", 1000 );
+      $( ".amount-tail" ).addClass( "amount-bass", 1000 );
+      $( ".rate-tail" ).addClass( "rate-bass", 1000 );
+
+      $( ".slider-AA" ).addClass( "a-attack-bass", 1000 );
+      $( ".slider-AD" ).addClass( "a-decay-bass", 1000 );
+      $( ".slider-AS" ).addClass( "a-sustain-bass", 1000 );
+      $( ".slider-AR" ).addClass( "a-release-bass", 1000 );
+
+      $( ".slider-FA" ).addClass( "f-attack-bass", 1000 );
+      $( ".slider-FD" ).addClass( "f-decay-bass", 1000 );
+      $( ".slider-FS" ).addClass( "f-sustain-bass", 1000 );
+      $( ".slider-FR" ).addClass( "f-release-bass", 1000 );
+
+
+
+
+
+
 
 });
-$( function() {
-  $( "#analog-bass" ).on( "click", function() {
-    $( ".osc1-tail" ).addClass( "move", 1000 );
-  });
-  });
+
 
 
 
   $("#digital-bass").on('click',function() {
-
     var $this = $(this),
         audio = $this.children('audio')[0];
         audio.play();
+
+
+        $( ".osc1-tail" ).addClass( "osc1-digital", 1000 );
+        $( ".osc2-tail" ).addClass( "osc2-digital", 1000 );
+        $( ".freq-tail" ).addClass( "freq-digital", 1000 );
+        $( ".res-tail" ).addClass( "res-digital", 1000 );
+        $( ".amount-tail" ).addClass( "amount-digital", 1000 );
+        $( ".rate-tail" ).addClass( "rate-digital", 1000 );
+
+        $( ".slider-AA" ).addClass( "a-attack-digital", 1000 );
+        $( ".slider-AD" ).addClass( "a-decay-digital", 1000 );
+        $( ".slider-AS" ).addClass( "a-sustain-digital", 1000 );
+        $( ".slider-AR" ).addClass( "a-release-digital", 1000 );
+
+        $( ".slider-FA" ).addClass( "f-attack-digital", 1000 );
+        $( ".slider-FD" ).addClass( "f-decay-digital", 1000 );
+        $( ".slider-FS" ).addClass( "f-sustain-digital", 1000 );
+        $( ".slider-FR" ).addClass( "f-release-digital", 1000 );
+
       });
