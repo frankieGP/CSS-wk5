288c41e072df1d0005cf1e44773ee6bb6aac5170
Frankie Gavin <frankanthonygavin@gmail.com>
2016-12-21 19:39:37 -0800

    onclick now moves correct div in the correct way.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
 $( document ).ready(function() {
 
-  $('[class*="osc1"]').on('click',function() {
+  $('[class*="osc1-tail"]').on('click',function() {
 
-  $(this).addClass("osc1tail");
+  $(this).addClass("move");
+  if($(this).hasClass("move")){
+  return;
+  }
+  $(this).addClass("move");
+  var that = this;
+  setTimeout(function(){
+  $(that).removeClass('move');
+},7250);
+});
 
-  });
+
   $("#analog-bass").click(function() {
-
-
-
     var $this = $(this),
         audio = $this.children('audio')[0];
         audio.play();
